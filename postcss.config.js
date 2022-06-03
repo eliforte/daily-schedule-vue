71bbf5a090b638f8107a449fab956695595c7d98
Elias Forte <eliasforte.dev@gmail.com>
2022-06-02 23:46:12 -0300

    fix: remove comments

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,7 @@
 /* eslint-disable */
-// https://github.com/michael-ciniawsky/postcss-load-config
 
 module.exports = {
   plugins: [
-    // https://github.com/postcss/autoprefixer
     require('autoprefixer')({
       overrideBrowserslist: [
         'last 4 Chrome versions',
@@ -16,12 +14,5 @@ module.exports = {
         'last 4 iOS versions'
       ]
     })
-
-    // https://github.com/elchininet/postcss-rtlcss
-    // If you want to support RTL css, then
-    // 1. yarn/npm install postcss-rtlcss
-    // 2. optionally set quasar.config.js > framework > lang to an RTL language
-    // 3. uncomment the following line:
-    // require('postcss-rtlcss')
   ]
 }
