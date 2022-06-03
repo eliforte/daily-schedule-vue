7797443a50fd5eaeca4ad5c0ad453812f1f7d6da
Elias Forte <eliasforte.dev@gmail.com>
2022-06-02 23:34:52 -0300

    feat: created `UserMutations`

---
+++ src/store/user/UserMutations.ts
@@ -0,0 +1,16 @@
+import { IUserState } from './UserTypes';
+
+export default {
+  setLoggedIn(state: IUserState, payload: boolean) {
+    state.loggedIn = payload;
+  },
+  setLoading(state: IUserState, payload: boolean) {
+    state.isLoading = payload;
+  },
+  setLoginForm(state: IUserState, payload: { key: keyof IUserState['loginForm'], value: string }) {
+    state.loginForm[payload.key] = payload.value;
+  },
+  setRegisterForm(state: IUserState, payload: { key: keyof IUserState['registerForm'], value: string }) {
+    state.registerForm[payload.key] = payload.value;
+  },
+};
