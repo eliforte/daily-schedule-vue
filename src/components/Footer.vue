deda3abd222bc9d546e4f2e279b54221eac2df64
Elias Forte <eliasforte.dev@gmail.com>
2022-06-03 18:16:28 -0300

    feat: component `Footer`

---
+++ src/components/Footer.vue
@@ -0,0 +1,45 @@
+<template>
+  <footer class="footer absolute-bottom">
+    <div class="container">
+      Developed by Elias Forte<a class="icon" href="https://github.com/eliforte/">
+        <img src="https://img.icons8.com/nolan/64/github.png"/>
+      </a>
+    </div>
+  </footer>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Footer-vue',
+  components: {},
+  methods: {},
+  computed: {},
+});
+</script>
+
+<style lang="sass">
+@import '../css/mixins'
+
+.footer
+  width: 100vw
+  height: 50px
+  display: flex
+  justify-content: center
+  position: absolute
+
+  .container
+    display: flex
+    justify-self: center
+    color: #fff
+    align-items: center
+.icon
+  img
+    width: 50px
+    height: 50px
+    margin: 0 10px
+    @include mobile-version
+      height: 100%
+
+</style>
