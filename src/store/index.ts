3092c0259dd87e213a3707b11249fbe387cc7893
Elias Forte <eliasforte.dev@gmail.com>
2022-06-07 16:20:53 -0300

    fix: name task module

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,12 @@
 import Vuex from 'vuex';
 import UserModule from './user/ModuleUser';
-import ModuleTasks from './task/ModuleTasks';
+import TasksModule from './task/ModuleTasks';
 import ResponseModule from './response/ModuleResponse';
 
 const store = new Vuex.Store({
   modules: {
     UserModule,
-    ModuleTasks,
+    TasksModule,
     ResponseModule,
   },
 });
