6fea97865ea3ff6ce7974b2c05ef0e5ee1b8a1ce
Elias Forte <eliasforte.dev@gmail.com>
2022-06-07 16:19:41 -0300

    feat: created component `Home`

---
+++ src/pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Oi
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Home-vue',
+});
+
+</script>
+
+<style lang="sass">
+
+</style>
