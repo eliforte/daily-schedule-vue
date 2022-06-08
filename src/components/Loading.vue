23b9c8f2a2c1d61ab21c77a8f007820c47cc1b62
Elias Forte <eliasforte.dev@gmail.com>
2022-06-08 18:10:35 -0300

    feat: created component `Loading`

---
+++ src/components/Loading.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <q-inner-loading
+      :showing="getIsLoading"
+      label="Please wait..."
+      label-class="text-deep-purple-10"
+      label-style="font-size: 1.1em"
+      color="deep-purple-10"
+    />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import { mapGetters } from 'vuex';
+
+export default defineComponent({
+  name: 'Loading-vue',
+  computed: {
+    ...mapGetters('UserModule', ['getIsLoading']),
+  },
+});
+</script>
+
+<style lang="sass">
+@import '../css/mixins'
+</style>
